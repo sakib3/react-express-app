f8367523e19abe3ab3d3b6d28d0101588a8ba86a
sakib3 <sakib3@gmail.com>
2015-11-08 17:09:13 +0100

    Using form

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -1,10 +1,20 @@
 var React = require('react/addons');
 module.exports = React.createClass({
+	getInitialState: function(){
+		return {input:""};
+	},
+	handleInputName: function(e){
+		this.setState({input: e.target.value});
+	},
+	addItem: function(e){
+		e.preventDefault();
+		console.log("Adding item!", this.state.input);
+	},
 	render: function (){
 		return (
 			<div className='grocery-addItem'>
-				<form>
-					<input />
+				<form onSubmit={this.addItem}>
+					<input value={this.props.input} onChange={this.handleInputName}/>
 					<button>Add Item</button>
 				</form>
 			</div>
