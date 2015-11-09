33d63e5df148c11745a8936ceab0ebc38d1adf71
sakib3 <sakib3@gmail.com>
2015-11-09 18:48:49 +0100

    implemented the mongo db and data is retriving from db

--- DB/database.js
+++ DB/database.js
@@ -1,5 +1,17 @@
 var mongoose = require('mongoose');
+var GroceryItem = require('./../models/GroceryItem.js');
 
 mongoose.connect('mongodb://localhost/grocery',function(){
 	console.log("Mongo DB is connected....");
+	mongoose.connection.db.dropDatabase();
+	var items = [
+	{name: "Candy"},
+	{name: "Milk"},
+	{name: "Sandwitch",purchased:true},
+	{name: "Drinks"}
+	];
+	// insert into the db
+	items.forEach(function(item){
+		new GroceryItem(item).save();
+	})
 })
--- routes/items.js
+++ routes/items.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var items = [
-	{name: "Candy"},
-	{name: "Milk"},
-	{name: "Sandwitch",purchased:true},
-	{name: "Drinks"}
-];
+var GroceryItem = require('./../models/GroceryItem.js');
+// var items = [
+// 	{name: "Candy"},
+// 	{name: "Milk"},
+// 	{name: "Sandwitch",purchased:true},
+// 	{name: "Drinks"}
+// ];
 
 /* GET items listing. */
 router
 .get('/', function(req, res) {
-  res.send(items);
+	GroceryItem.find(function(error, doc){
+  		res.send(doc);
+		
+	})
 })
 .post('/', function(req, res) {
   var item = req.body;
-  items.push(item);
+  var groceryItem = new GroceryItem(item);
+  groceryItem.save(function(err,data){
+  	//ok send
+  	res.status(300).send();
+  	
+  })
 });
 
 
